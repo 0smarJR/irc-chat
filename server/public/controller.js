57b4fbef7396822d2914d9fcf0d6d83cecab478f
0smarJR <osmar.emidio.junior@gmail.com>
2020-10-05 01:46:29 -0300

    Criação do esquema de salas

---
+++ server/public/controller.js
@@ -0,0 +1,62 @@
+// Local no qual o servidor está hospedado
+const socket = io('http://localhost:3030');
+
+// Pegando elementos do HTML
+const messageForm = document.getElementById('send-container');
+const messageInput = document.getElementById('message-input');
+const messageContainer = document.getElementById('message-container');
+const roomContainer = document.getElementById('room-container');
+
+
+//Verificando se o form para novo usuario é solicitado na página (verificação feita para evitar que esse form apareça na pagina index)
+if(messageForm != null){
+    // Nome do usuário
+    const name= prompt('Qual o seu nome?')
+    appendMessage('Você entrou')
+    // Display do usuário que entrou
+    socket.emit('new-user', roomName, name)
+    // Submit button
+    messageForm.addEventListener('submit', e => {
+        e.preventDefault()
+        const message = messageInput.value;
+        appendMessage(`Você: ${message}`)
+        // Enviar mensagem para o servidor
+        socket.emit('send-chat-message', roomName, message)
+        // Limpando input apos enviar
+        messageInput.value = '';
+    })
+}
+
+//Criando nova sala
+socket.on('room-created', room => {
+    const roomElement = document.createElement('div')
+    roomElement.innerText = room
+    const roomLink = document.createElement('a')
+    roomLink.href = `/${room}`
+    roomLink.innerText = 'Entrar'
+    roomContainer.append(roomElement)
+    roomContainer.append(roomLink)
+})
+
+socket.on('chat-message', data => {
+    // Diplay da Mensagem no cliente
+    appendMessage(`${data.name}: ${data.message}`)
+})
+
+
+// Usuário conectado
+socket.on('user-connected', name => {
+    appendMessage(`${name} Conectou`)
+})
+
+// Usuário desconectado
+socket.on('user-disconnected', name => {
+    appendMessage(`${name} Desconectou`)
+})
+
+// Adicionando a mensagem ao cliente
+function appendMessage(message) {
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageContainer.append(messageElement);
+}
--- server/server.js
+++ server/server.js
@@ -1,28 +1,72 @@
-const io = require('socket.io')(3030)
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+app.set('views', './client') //Definindo pasta com as paginas usadas
+app.set('view engine', 'ejs') //Definindo arquivos de pagina como .ejs em vez de .html
+app.use(express.static('public')) //Definindo pasta onde ficará o controller
+app.use(express.urlencoded({ extended: true }))
+
+const rooms = {}
+
+//Redirecionamento da url '/' para a pagina index e passando as salas como parametro
+app.get('/', (req, res) => {
+    res.render('index', {rooms: rooms})
+})
+
+//Formulario de criação de nova sala
+app.post('/room', (req, res) => {
+    //Verificando se já não existe uma sala com mesmo nome
+    if (rooms[req.body.room] != null) {
+        return res.redirect('/')
+      }
+      rooms[req.body.room] = { users: {} }
+      res.redirect(req.body.room)
+    //Mensagem informando criação da sala
+    io.emit('room-created', req.body.room)
+})
+
+//Criando uma url para cada sala
+app.get('/:room', (req, res) => {
+    //Verificação para impedir que a pessoa entre em uma sala que não existe
+    if (rooms[req.params.room] == null){
+        return res.redirect('/')
+    }
+    res.render('room', { roomName: req.params.room})
+})
+
+server.listen(3030)
 
 // Banco de dados
-const users = {};
-
 
 io.on('connection', socket => {
-
     // Conectando usuário
-    socket.on('new-user', name => {
+    socket.on('new-user', (room, name) => {
         //Definindo ID único para usuário
-        users[socket.id] = name;
-        socket.broadcast.emit('user-connected', name)
+        socket.join(room)
+        rooms[room].users[socket.id] = name
+        socket.to(room).broadcast.emit('user-connected', name)
     })
 
     // Tratando mensagem recebida
-    socket.on('send-chat-message', message => {
-        // Enviando a mensagem para os outros usuários
-        socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
-    })
-
+    socket.on('send-chat-message', (room, message) => {
+        // Enviando a mensagem para os outros usuários de uma determinada sala
+        socket.to(room).broadcast.emit('chat-message', { message: message, name: rooms[room].users[socket.id] })
+      })
     // Desconenctando usuário
     socket.on('disconnect', () => {
-        socket.broadcast.emit('user-disconnected', users[socket.id])
-        // Deletando usuários do array
-        delete users[socket.id]
-    });
+        getUserRooms(socket).forEach(room => {
+          socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id])
+          delete rooms[room].users[socket.id]
+        })
+    })
 })
+
+//Função para listar todas as salas e os usuarios que se encontram nela
+function getUserRooms(socket) {
+    return Object.entries(rooms).reduce((names, [name, room]) => {
+      if (room.users[socket.id] != null) names.push(name)
+      return names
+    }, [])
+  }
